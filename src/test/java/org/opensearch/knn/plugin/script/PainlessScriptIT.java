4617a83ae224dfdee5055fcd8cb33ab7cce6f066
Ryan Bogan <rbogan@amazon.com>
2024-04-29 14:06:35 -0700

    Revert 'Support script score when doc value is disabled'
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- src/test/java/org/opensearch/knn/plugin/script/PainlessScriptIT.java
+++ src/test/java/org/opensearch/knn/plugin/script/PainlessScriptIT.java
@@ -565,7 +565,6 @@ public class PainlessScriptIT extends KNNRestTestCase {
         properties.add(
             new MappingProperty(FIELD_NAME, KNNVectorFieldMapper.CONTENT_TYPE).dimension("2")
                 .knnMethodContext(knnMethodContext)
-                .docValues(randomBoolean())
         );
 
         String source = String.format("1/(1 + l2Squared([1.0f, 1.0f], doc['%s']))", FIELD_NAME);
